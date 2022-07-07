21061b6926f6240c0c9c97810b373666ae61ca85
darkkroast <83790984+colburnl@users.noreply.github.com>
2022-07-07 01:25:57 -0400

    Delete grab-device.sh

--- grab-device.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "========================================================"
-echo
-echo "welcome! build-adroid builder beta-version"
-echo
-echo "this simple builder will ask a few questions, configure then build."
-echo "plently of options to choose from, continue?"
-read -p continue
